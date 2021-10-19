4597ff4902f483f97238275b38c0748ffd29e5fa
jiachengng <70906604+jiachengng@users.noreply.github.com>
2021-10-19 10:02:04 +0800

    .

--- MultiThreadingCamera/Program.cs
+++ MultiThreadingCamera/Program.cs
@@ -16,23 +16,21 @@ namespace Multi_Threading_Camera
         public static void ChildThread1()
         {
             Console.WriteLine("Child 1 thread starts");
-
-            VideoCapture capture = new VideoCapture(); //create a camera capture
+            VideoCapture capture1 = new VideoCapture(0); //create a camera capture
             Console.WriteLine("Child thread 1 created camera capture");
-            Bitmap image1 = capture.QueryFrame().ToBitmap(); //take a picture
+            Bitmap image1 = capture1.QueryFrame().ToBitmap(); //take a picture
             Console.WriteLine("Child thread 1 took a photo");
             //Saving photos into folder
             string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
-            image1.Save(FileName + ".jpg");
+            image1.Save(FileName +"1" + ".jpg");
             Console.WriteLine("Child thread 1 saved an image in the folder");
         }
         public static void ChildThread2()
         {
             Console.WriteLine("Child 2 thread starts");
-
-            VideoCapture capture = new VideoCapture(1); //create a camera capture
+            VideoCapture capture2 = new VideoCapture(1); //create a camera capture
             Console.WriteLine("Child thread 2 created camera capture");
-            Bitmap image2 = capture.QueryFrame().ToBitmap(); //take a picture
+            Bitmap image2 = capture2.QueryFrame().ToBitmap(); //take a picture
             Console.WriteLine("Child thread 2 took a photo");
             // the thread is paused for 1500 milliseconds
             //int sleepfor = 1500;
@@ -42,21 +40,20 @@ namespace Multi_Threading_Camera
             //Console.WriteLine("Child thread 2 resumes");
 
             //Saving photos into folder
-            string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
-            image2.Save(FileName + ".jpg");
+            string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads",DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image2.Save(FileName + "2" + ".jpg");
             Console.WriteLine("Child thread 2 saved an image in the folder");
         }
 
         public static void ChildThread3()
         {
             Console.WriteLine("Child 3 thread starts");
-
-            VideoCapture capture = new VideoCapture(2); //create a camera capture
+            VideoCapture capture3 = new VideoCapture(2); //create a camera capture
             Console.WriteLine("Child thread 3 created camera capture");
-            Bitmap image3 = capture.QueryFrame().ToBitmap(); //take a picture
+            Bitmap image3 = capture3.QueryFrame().ToBitmap(); //take a picture
             Console.WriteLine("Child thread 3 took a photo");
             // the thread is paused for 1500 milliseconds
-            //int sleepfor = 1500;
+            //int sleepfor = 1000;
 
             //Console.WriteLine("Child Thread 2 Paused for {0} seconds", sleepfor / 1000);
             //Thread.Sleep(sleepfor);
@@ -64,29 +61,53 @@ namespace Multi_Threading_Camera
 
             //Saving photos into folder
             string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
-            image3.Save(FileName + ".jpg");
+            image3.Save(FileName + "3" + ".jpg");
             Console.WriteLine("Child thread 3 saved an image in the folder");
+        }
+
+        public static void ChildThread4()
+        {
+            Console.WriteLine("Child 4 thread starts");
+            VideoCapture capture4 = new VideoCapture(3); //create a camera capture
+            Console.WriteLine("Child thread 4 created camera capture");
+            Bitmap image4 = capture4.QueryFrame().ToBitmap(); //take a picture
+            Console.WriteLine("Child thread 4 took a photo");
+
+            //Saving photos into folder
+            //Thread.Sleep(1000);
+            string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image4.Save(FileName + "4" + ".jpg");
+            Console.WriteLine("Child thread 4 saved an image in the folder");
         }
 
         static void Main(string[] args)
         {
+            int start = 1;
+            while (start == 1)
+            {
+                System.Threading.ThreadStart childref1 = new ThreadStart(ChildThread1);
+                System.Threading.ThreadStart childref2 = new ThreadStart(ChildThread2);
+                System.Threading.ThreadStart childref3 = new ThreadStart(ChildThread3);
+                System.Threading.ThreadStart childref4 = new ThreadStart(ChildThread4);
 
-            System.Threading.ThreadStart childref1 = new ThreadStart(ChildThread1);
-            System.Threading.ThreadStart childref2 = new ThreadStart(ChildThread2);
-            System.Threading.ThreadStart childref3 = new ThreadStart(ChildThread3);
+                //Console.WriteLine("In Main: Creating the Child threads");
+                Thread childThread1 = new Thread(childref1);
+                childThread1.Start();
 
-            Console.WriteLine("In Main: Creating the Child threads");
+                Thread childThread2 = new Thread(childref2);
+                childThread2.Start();
 
-            Thread childThread1 = new Thread(childref1);
-            childThread1.Start();
+                Thread childThread3 = new Thread(childref3);
+                childThread3.Start();
 
-            Thread childThread2 = new Thread(childref2);
-            childThread2.Start();
+                Thread childThread4 = new Thread(childref4);
+                childThread4.Start();
 
-            Thread childThread3 = new Thread(childref3);
-            childThread3.Start();
-
-            Console.ReadKey();
+                Console.Write("Another image? (1 or 0): ");
+                start = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            
         }
     }
 }
